c99e8a492bacb84f3c80e76a4a6c64b4892c01e7
Dm Ant <Dm@Ant.ru>
2023-03-16 12:43:00 +0300

    session-2 CodeFromEmailActivity сделан переход по полям при вводе цифры

--- app/src/main/java/com/example/profi23/CodeFromEmailActivity.java
+++ app/src/main/java/com/example/profi23/CodeFromEmailActivity.java
@@ -3,12 +3,98 @@ package com.example.profi23;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 public class CodeFromEmailActivity extends AppCompatActivity {
+
+    EditText codeNum1, codeNum2, codeNum3, codeNum4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_code_from_email);
+        codeNum1 = findViewById(R.id.email_code_num1);
+        codeNum2 = findViewById(R.id.email_code_num2);
+        codeNum3 = findViewById(R.id.email_code_num3);
+        codeNum4 = findViewById(R.id.email_code_num4);
+
+        //сделаем переход по полям ввода после ввода в них цифр из кода, полученного по email
+        codeNum1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(editable.length() == 1){
+                    codeNum2.requestFocus();
+                }
+            }
+        });
+        codeNum2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(editable.length() == 1){
+                    codeNum3.requestFocus();
+                }
+            }
+        });
+        codeNum3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(editable.length() == 1){
+                    codeNum4.requestFocus();
+                }
+            }
+        });
+        //после ввода цифры в последнее окно, сделаем запрос к серверу на проверку корректности
+        // ввода кода из email
+        codeNum4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                //если введена последняя цифра, то делаем запрос
+                if(editable.length() == 1){
+
+                }
+            }
+        });
     }
 }
