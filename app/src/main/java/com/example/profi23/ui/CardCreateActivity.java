2ea130403adc27dd9e592d4a6d10ccc4a5d58907
Dm Ant <Dm@Ant.ru>
2023-03-16 17:49:17 +0300

    session-2 activity_pswrd_create.xml v2

---
+++ app/src/main/java/com/example/profi23/ui/CardCreateActivity.java
@@ -0,0 +1,19 @@
+package com.example.profi23.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.profi23.R;
+
+public class CardCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_create);
+        getSupportActionBar().hide();
+
+
+    }
+}
--- app/src/main/java/com/example/profi23/ui/PswrdCreateActivity.java
+++ app/src/main/java/com/example/profi23/ui/PswrdCreateActivity.java
@@ -2,16 +2,50 @@ package com.example.profi23.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.profi23.R;
 
 public class PswrdCreateActivity extends AppCompatActivity {
+
+    private static final String APP_PREFERENCES = "preferences";
+    private static final String APP_PREFERENCES_PSWRD_STATUS = "pswrd_status";
+    private static final String APP_PREFERENCES_PSWRD_VALUE = "pswrd";
+
+    private int pswrd;
+    TextView skipCreatePswrd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pswrd_create);
         getSupportActionBar().hide();
+
+        skipCreatePswrd = findViewById(R.id.skip_create_pswrd);
+
+        //сохраним пароль и информацию о том, был ли пароль создан
+        SharedPreferences preferences = getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE);
+        boolean isPswrdCreate = preferences.getBoolean(APP_PREFERENCES_PSWRD_STATUS, false);
+        if (isPswrdCreate){
+            startActivity(new Intent(PswrdCreateActivity.this, CardCreateActivity.class));
+        }
+
+        skipCreatePswrd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putBoolean(APP_PREFERENCES_PSWRD_STATUS, false);
+                editor.commit();
+            }
+        });
+
+
+
     }
 }
