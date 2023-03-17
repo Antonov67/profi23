9c31a138392e335b42a0075cbc18baefcf4548e9
Dm Ant <Dm@Ant.ru>
2023-03-17 13:14:39 +0300

    session-2 CardCreateActivity

--- app/src/main/java/com/example/profi23/ui/CardCreateActivity.java
+++ app/src/main/java/com/example/profi23/ui/CardCreateActivity.java
@@ -3,10 +3,16 @@ package com.example.profi23.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.profi23.R;
 
 public class CardCreateActivity extends AppCompatActivity {
+
+    Spinner spinner;
+
+    private static final String[] pol = {"Мужской", "Женский"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +20,11 @@ public class CardCreateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_card_create);
         getSupportActionBar().hide();
 
+        spinner = findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(CardCreateActivity.this,
+                android.R.layout.simple_spinner_item,pol);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
     }
 }
