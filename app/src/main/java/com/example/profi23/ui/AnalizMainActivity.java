d51664443a677e18eb2c93ceb887c37a3f7fd096
Dm Ant <Dm@Ant.ru>
2023-03-17 18:15:44 +0300

    session-3 AnalizMainActivity step1

--- app/src/main/java/com/example/profi23/ui/AnalizMainActivity.java
+++ app/src/main/java/com/example/profi23/ui/AnalizMainActivity.java
@@ -13,5 +13,6 @@ public class AnalizMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analiz_main);
         getSupportActionBar().hide();
+
     }
 }
