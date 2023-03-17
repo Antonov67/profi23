141789788682b5e4f900cdd4943563fccbea0a75
Dm Ant <Dm@Ant.ru>
2023-03-17 17:10:56 +0300

    session-3 AnalizMainActivity start

---
+++ app/src/main/java/com/example/profi23/ui/AnalizMainActivity.java
@@ -0,0 +1,17 @@
+package com.example.profi23.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.profi23.R;
+
+public class AnalizMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analiz_main);
+        getSupportActionBar().hide();
+    }
+}
--- app/src/main/java/com/example/profi23/ui/CardCreateActivity.java
+++ app/src/main/java/com/example/profi23/ui/CardCreateActivity.java
@@ -3,12 +3,17 @@ package com.example.profi23.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.profi23.R;
@@ -30,7 +35,8 @@ public class CardCreateActivity extends AppCompatActivity {
 
     private Spinner spinner;
     private MaterialButton createButton;
-    EditText name, middleName, surname, birthday;
+    private EditText name, middleName, surname, birthday;
+    private TextView skipCardCreate;
 
     private static final String[] pol = {"Мужской", "Женский"};
 
@@ -52,6 +58,15 @@ public class CardCreateActivity extends AppCompatActivity {
         middleName = findViewById(R.id.editText_middle_name);
         surname = findViewById(R.id.editText_surname);
         birthday = findViewById(R.id.editText_birthday);
+
+        //по нажатию кнопки пропустить, переходим к активити с анализами
+        skipCardCreate = findViewById(R.id.skip_create_card);
+        skipCardCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CardCreateActivity.this, AnalizMainActivity.class));
+            }
+        });
 
 
         //кнопка создания карты пациента
@@ -79,18 +94,49 @@ public class CardCreateActivity extends AppCompatActivity {
 
                     @Override
                     public void onFailure(Call<ResponseServer> call, Throwable t) {
-
                     }
                 });
                 Log.d("777","token: " +  Utils.token);
                 Log.d("777", profile.toString());
-
-
-
-
-
             }
         });
+
+        //сделаем блокировку нажатия кнопки и изменение ее цвета, если не все поля заполнены
+        //для этого реализуем свой класс, чтобы не дублировать код (принцип DRY)
+
+        class MyTextWatcher implements TextWatcher{
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                //если хотя бы одно поле пустое, то  кнопка не активна
+                if (name.length() == 0 || surname.length() == 0 || middleName.length() == 0 || birthday.length() == 0){
+                    int color = Color.parseColor("#C9D4FB");
+                    createButton.setBackgroundColor(color);
+                    createButton.setClickable(false);
+                }else {
+                    int color = Color.parseColor("#1A6FEE");
+                    createButton.setBackgroundColor(color);
+                    createButton.setClickable(true);
+                }
+            }
+        }
+
+        //и добавим этот обработчик в поля ввода
+        name.addTextChangedListener(new MyTextWatcher());
+        surname.addTextChangedListener(new MyTextWatcher());
+        middleName.addTextChangedListener(new MyTextWatcher());
+        birthday.addTextChangedListener(new MyTextWatcher());
+
 
 
     }
